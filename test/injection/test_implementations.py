edab9671c3c56ea9c972c258d980df7c5a44382e
Filip Malczak <filip.malczak@gmail.com>
2024-09-05 18:35:33 +0200

    Add another implementations test

--- test/injection/test_implementations.py
+++ test/injection/test_implementations.py
@@ -16,6 +16,28 @@ def test_fixture_impls():
     })
     assert expected == impls
 
+class ConcreteParent: pass
+
+class ConcreteChild1(ConcreteParent): pass
+
+class ConcreteChild2(ConcreteParent): pass
+
+class ConcreteGrandchild11(ConcreteChild1): pass
+
+class ConcreteGrandchild12(ConcreteChild1): pass
+
+
+@case
+def test_concrete_hierarchy_with_default_primary():
+    impls = Implementations.build({ConcreteParent, ConcreteChild1, ConcreteChild2, ConcreteGrandchild11, ConcreteGrandchild12})
+    expected = Implementations({
+        ConcreteParent: ImplementationDetails(frozenset([ConcreteChild1, ConcreteChild2, ConcreteGrandchild11, ConcreteGrandchild12]), ConcreteParent),
+        ConcreteChild1: ImplementationDetails(frozenset([ConcreteChild1, ConcreteGrandchild11, ConcreteGrandchild12]), ConcreteChild1),
+        ConcreteChild2: ImplementationDetails(frozenset([ConcreteChild2]), ConcreteChild2),
+        ConcreteGrandchild11: ImplementationDetails(frozenset([ConcreteGrandchild11]), ConcreteGrandchild11),
+        ConcreteGrandchild12: ImplementationDetails(frozenset([ConcreteGrandchild12]), ConcreteGrandchild12)
+    })
+
 #simple subtype hierarchy of concrete classes
 #ditto, but use @primary
 #hierarchy of interface impls
