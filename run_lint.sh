997e920a3a5f1d46fafdcb21a19762c228817596
Filip Malczak <filip.malczak@gmail.com>
2024-11-03 15:37:46 +0100

    Exclude calculator from linting

--- run_lint.sh
+++ run_lint.sh
@@ -1,2 +1,7 @@
 # YOU MUST ACTIVATE VENV ON YOUR OWN IF YOU USE THIS LOCALLY.
-prospector --full-pep8 --with-tool vulture -o text:./lint_report.txt -o xunit:./lint_report.xml --zero-exit
+prospector --full-pep8 \
+  --with-tool vulture \
+  --ignore-paths calculator \
+  -o text:./lint_report.txt \
+  -o xunit:./lint_report.xml \
+  --zero-exit
