b2ef885f1d7454fd083fae05f8dfb4c144a2d278
Filip Malczak <filip.malczak@gmail.com>
2025-02-18 16:45:28 +0100

    Fix duration checks by counting second (and not nanoseconds)

--- clustering/test.sh
+++ clustering/test.sh
@@ -11,24 +11,19 @@ source ./venv/bin/activate >/dev/null 2>/dev/null
 export SMALL_DATASET=1
 export SHORT_RUN=1
 
-# if you're running on MacOS, you may wanna use gdate instead (after installing coreutils)
-# otherwise it may not support %N (nanoseconds)
-# in GH Actions the MacOS runner should support it anyway
-
 echo "First run - it should actually execute the experiment"
-BEFORE_FIRST=$(date +%s%N)
+BEFORE_FIRST=$(date +%s)
 python3 ./app.py
-AFTER_FIRST=$(date +%s%N)
+AFTER_FIRST=$(date +%s)
 FIRST_DURATION=$(( AFTER_FIRST-BEFORE_FIRST ))
 FIRST_DURATION_MS=$(( FIRST_DURATION/1000000 ))
 echo "First run took ${FIRST_DURATION_MS}ms"
 
 echo "Second run - shouldn't execute any steps, just confirm that stages structure hasn't changed"
-BEFORE_SECOND=$(date +%s%N)
+BEFORE_SECOND=$(date +%s)
 python3 ./app.py
-AFTER_SECOND=$(date +%s%N)
+AFTER_SECOND=$(date +%s)
 SECOND_DURATION=$(( AFTER_SECOND-BEFORE_SECOND ))
-SECOND_DURATION_MS=$(( SECOND_DURATION/1000000 ))
 echo "Second run took ${SECOND_DURATION_MS}ms"
 
 if [[ $SECOND_DURATION -gt $FIRST_DURATION ]]; then
