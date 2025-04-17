898020afd927d9f8240a4e2c90183843b9c3e625
Filip Malczak <filip.malczak@gmail.com>
2025-04-17 14:23:59 +0200

    Test Serializable support in JSON-like columns

--- test/executor_data/dolt/test_jsonish_columns.py
+++ test/executor_data/dolt/test_jsonish_columns.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from random import Random
 from types import GenericAlias
 from uuid import uuid4
@@ -11,6 +12,7 @@ from thinking_executor_data.dolt.sqlalchemy.base import JSON, BSON, Pickle, SqlA
 from thinking_executor_data.dolt.sqlalchemy.storage import DoltStorage
 from thinking_injection.context.configurable.impl import ConfigurableContext
 from thinking_injection.typeset import from_packages
+from thinking_programming.serialization import Serializable, CustomSerializable, SerializableMixin
 from thinking_programming.tracing import traced
 
 #todo change seed per run? or maybe run each test twice - with constant seed and with timestamp-based seed?
@@ -22,14 +24,28 @@ JSON_LIKE_COLUMN_TYPES = [
     Pickle
 ]
 
-PRIMITIVE_TYPE_RANDOMIZERS = {
+@dataclass
+class SimpleSerializable(SerializableMixin):
+    txt: str
+    i: int
+
+#todo more love for serializable; at least the case where it has list[int] field and another with dict
+
+RANDOMIZERS = {
     int: lambda: RANDOM.randint(0, 1000),
     float: lambda: RANDOM.random(),
     bool: lambda: RANDOM.random() > 0.5,
-    str: lambda: str(uuid4())
+    str: lambda: str(uuid4()),
+    SimpleSerializable: lambda: SimpleSerializable(random_value(str), random_value(int))
 }
 
-PRIMITVE_TYPES = list(PRIMITIVE_TYPE_RANDOMIZERS.keys())
+PRIMITVE_TYPES = [
+    int,
+    float,
+    bool,
+    str
+]
+SIMPLE_TYPES = PRIMITVE_TYPES + [ SimpleSerializable ]
 
 def construct_type_variants(t):
     return [
@@ -40,8 +56,8 @@ def construct_type_variants(t):
     ]
 
 def random_value(t):
-    if t in PRIMITVE_TYPES:
-        return PRIMITIVE_TYPE_RANDOMIZERS[t]()
+    if t in RANDOMIZERS:
+        return RANDOMIZERS[t]()
     assert isinstance(t, GenericAlias), f"Actually: {t}"
     container_type = t.__origin__
     assert container_type in [list, dict]
@@ -53,7 +69,7 @@ def random_value(t):
 
 
 def to_class_name(ValueT: type):
-    if ValueT in PRIMITVE_TYPES:
+    if ValueT in SIMPLE_TYPES:
         return ValueT.__name__
     if ValueT.__origin__ == list:
         return "list_of_"+to_class_name(ValueT.__args__[0])
@@ -71,7 +87,7 @@ def entity_with_column(ColumnT: type, entity_name: str) -> type:
     )
     return result
 
-def fixture(EntityType, variant):
+def given_entity_can_store_given_data(EntityType, data_type):
     ctx = ConfigurableContext([
         *from_packages("thinking_executor", "thinking_executor_data.common", "thinking_executor_data.dolt")
     ])
@@ -83,30 +99,28 @@ def fixture(EntityType, variant):
         def top_level():
             @executor.step
             def a_step():
-                saved = repo.save_one(EntityType(data=random_value(variant)))
+                saved = repo.save_one(EntityType(data=random_value(data_type)))
                 assert repo.count().all() == 1
                 assert [x.id_ for x in repo.find().all()] == [saved.id_]
 
 for JsonLikeT in JSON_LIKE_COLUMN_TYPES:
     RawEntityType = entity_with_column(JsonLikeT, "Entity_with_" + JsonLikeT.__name__)
-    for PrimitiveT in PRIMITVE_TYPES:
-        for VariantT in construct_type_variants(PrimitiveT):
+    for SimpleT in SIMPLE_TYPES:
+        for VariantT in construct_type_variants(SimpleT):
             SpecializedEntityType = entity_with_column(
                 JsonLikeT[VariantT],
                 "Entity_with_" + JsonLikeT.__name__ + "_over_" + to_class_name(VariantT)
             )
 
-            @parametrized_case(params=(RawEntityType, VariantT))
-            def raw_json_like_works_with_any_type_variant(raw_type, variant):
-                fixture(raw_type, variant)
+            if SimpleT in PRIMITVE_TYPES:
+                @parametrized_case(params=(RawEntityType, VariantT))
+                def raw_json_like_works_with_any_primitive_type_variant(raw_type, variant):
+                    given_entity_can_store_given_data(raw_type, variant)
 
             @parametrized_case(params=(SpecializedEntityType, VariantT))
             def specialized_json_like_works_with_matching_variant(specialized_type, variant):
-                fixture(specialized_type, variant)
+                given_entity_can_store_given_data(specialized_type, variant)
 
-
-
-#todo cover Serializable support
 
 if __name__=="__main__":
     run_current_module()
